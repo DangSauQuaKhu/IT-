44327282d1702f482e49af4d509a37e9432ced4d
DangSauQuaKhu <tklmissno1@gmail.com>
2023-02-06 16:41:16 +0000

    add profile front

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,7 +4,7 @@ class UserController < ApplicationController
   end
   def show
      @users = User.find(params[:format])
-     @blogs =  Blog.where(["email like ?", "%#{@users.email}%"]).paginate(:page => params[:page], :per_page =>6) 
+     @blogs =  Blog.where(["email like ?", "%#{@users.email}%"]).paginate(:page => params[:page], :per_page =>4) 
   end
   def user_params
       params.require(:user).permit(:email, :password, :name,:avatar,:password_confirmation, :age)
